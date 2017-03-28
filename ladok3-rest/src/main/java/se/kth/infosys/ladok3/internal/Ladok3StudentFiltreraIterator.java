94b79684e01944fe9c56b38fd20a5e593d23d08a
Fredrik Jönsson <fjo@kth.se>
2017-03-28 14:47:40 +0200

    Refactor code.

--- ladok3-rest/src/main/java/se/kth/infosys/ladok3/internal/Ladok3StudentFiltreraIterator.java
+++ ladok3-rest/src/main/java/se/kth/infosys/ladok3/internal/Ladok3StudentFiltreraIterator.java
@@ -4,12 +4,49 @@ import java.util.Iterator;
 import java.util.Map;
 
 import se.kth.infosys.ladok3.Ladok3StudentInformationService;
+import se.ladok.schemas.studentinformation.SokresultatStudentinformationRepresentation;
 import se.ladok.schemas.studentinformation.StudentISokresultat;
 
-public class Ladok3StudentFiltreraIterator extends Ladok3StudentFiltreraIteratorBase implements Iterator<StudentISokresultat> {
-    public Ladok3StudentFiltreraIterator(Ladok3StudentInformationService ladok3StudentInformationService,
+public class Ladok3StudentFiltreraIterator implements Iterator<StudentISokresultat> {
+    protected Ladok3StudentInformationService service;
+    protected SokresultatStudentinformationRepresentation result;
+    protected Iterator<StudentISokresultat> iterator;
+
+    private Map<String, Object> params;
+    private int page = 0;
+    private int limit = 400;
+
+    public Ladok3StudentFiltreraIterator(
+            Ladok3StudentInformationService ladok3StudentInformationService,
             Map<String, Object> params) {
-        super(ladok3StudentInformationService, params);
+        this.service = ladok3StudentInformationService;
+        this.params = params;
+
+        if (params.get("limit") != null) {
+            if (params.get("limit") instanceof Integer) {
+                this.limit = (Integer) params.get("limit");
+            } else {
+                this.limit = Integer.parseInt((String) params.get("limit"));
+            }
+        }
+
+        getNextPage();
+    }
+
+    protected void getNextPage() {
+        params.put("limit", limit);
+        params.put("page", ++page);
+        result = service.studentFiltrera(params);
+        iterator = result.getResultat().iterator();
+    }
+
+   @Override 
+    public boolean hasNext() {
+        return iterator.hasNext() || serviceHasNext();
+    }
+
+    protected boolean serviceHasNext() {
+        return result.getResultat().size() == limit;
     }
 
     @Override
--- ladok3-rest/src/main/java/se/kth/infosys/ladok3/internal/Ladok3StudentFiltreraStudentIterator.java
+++ ladok3-rest/src/main/java/se/kth/infosys/ladok3/internal/Ladok3StudentFiltreraStudentIterator.java
@@ -5,21 +5,28 @@ import java.util.Map;
 
 import se.kth.infosys.ladok3.Ladok3StudentInformationService;
 import se.ladok.schemas.studentinformation.Student;
+import se.ladok.schemas.studentinformation.StudentISokresultat;
 
-public class Ladok3StudentFiltreraStudentIterator extends Ladok3StudentFiltreraIteratorBase implements Iterator<Student> {
+public class Ladok3StudentFiltreraStudentIterator implements Iterator<Student> {
+    private Ladok3StudentInformationService service;
+    private Iterator<StudentISokresultat> iterator;
+
     public Ladok3StudentFiltreraStudentIterator(Ladok3StudentInformationService ladok3StudentInformationService,
             Map<String, Object> params) {
-        super(ladok3StudentInformationService, params);
+        this.service = ladok3StudentInformationService;
+        this.iterator = service.studentFiltreraIterator(params);
     }
 
     @Override
     public Student next() {
         if (iterator.hasNext()) {
             return service.studentUID(iterator.next().getUid());
-        } else if (serviceHasNext()) {
-            getNextPage();
-            return service.studentUID(iterator.next().getUid());
         }
         return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return iterator.hasNext();
     }
 }
