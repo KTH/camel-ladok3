7f9890c93ddf0ea49491ecb091d096b97e760d13
Marcus Götesson <marcus@gothius.se>
2020-05-19 10:55:39 +0200

    new schema version 1.48.1

--- ladok3-model/src/main/resources/download.sh
+++ ladok3-model/src/main/resources/download.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-files=legacy
-#files=files
+legacy=legacy
+files=files
+dest=schemas
 
 function fail() {
     echo $*
     echo
     echo "Usage:"
-    echo "$0: <name of session cookie> <value of session cookie> [destination]"
-    echo
-    echo "  destination: a destination directory, i.e. schemas, the current directory by default"
+    echo "$0: <name of session cookie> <value of session cookie>"
     echo
 }
 
@@ -25,11 +24,13 @@ fi
 
 cookie="$1=$2"
 
-if [ ! -z "$3" ]; then
-    mkdir -p "$3"
-fi
-
 for url in $(cat ${files}); do
     file=${url##*/}
-    curl -b "${cookie}" $url -o ${3:-.}/$file
+    curl -b "${cookie}" $url -o ${dest}/$file
+done
+
+# legacy
+for url in $(cat ${legacy}); do
+    file=${url##*/}
+    curl -b "${cookie}" $url -o ${dest}/legacy/$file
 done
