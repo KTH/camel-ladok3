32be6f0a767f2fe5f4c4bd454cee1aaba18bd3db
Marcus Götesson <marcus@gothius.se>
2021-03-02 09:33:14 +0100

    Removed deprecated unmarshaller method.

--- camel-ladok3-component/src/main/java/se/kth/infosys/smx/ladok3/internal/Ladok3UnmarshallerFactory.java
+++ camel-ladok3-component/src/main/java/se/kth/infosys/smx/ladok3/internal/Ladok3UnmarshallerFactory.java
@@ -9,20 +9,6 @@ import javax.xml.bind.Unmarshaller;
 public class Ladok3UnmarshallerFactory {
   private static final Map<String, Unmarshaller> unmarshallers = new HashMap<>();
 
-  /**
-   * @deprecated Since the split of the Ladok model into api- and event-jars, there are ObjectFactories with identical
-   * FQCNs. So, if you for example send in "se.ladok.schema.resultat" and have the ladok-api-jar before
-   * the event-jar in the CP, the ObjectFactory from the api-jar will be used and no Events can be unmarshalled.
-   */
-  @Deprecated
-  public static Unmarshaller unmarshaller(String context) throws JAXBException {
-    if (!unmarshallers.containsKey(context)) {
-      JAXBContext jaxbContext = JAXBContext.newInstance(context);
-      unmarshallers.put(context, jaxbContext.createUnmarshaller());
-    }
-    return unmarshallers.get(context);
-  }
-
   public static Unmarshaller unmarshaller(String context, Class<?> eventClass) throws JAXBException {
     if (!unmarshallers.containsKey(context)) {
       JAXBContext jaxbContext = JAXBContext.newInstance(eventClass);
